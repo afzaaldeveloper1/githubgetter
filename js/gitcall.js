4041d31efdc75a1acd08accc2fbf3557408b2939
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-23 01:52:34 +0000

    Added a function that handles the output or display of info after calling fir info from github api

--- js/gitcall.js
+++ js/gitcall.js
@@ -7,3 +7,34 @@ $(function(){
     var main   = 'https://api.github.com/users/'+username;
     var repourl  = 'https://api.github.com/users/'+username+'/repos';
   }
+  getInfo(main, function(data) {
+      if(data.message == "Not Found" || username == '') {
+        $('#gallery').html("<h2>No User Info Found</h2>");
+      }
+      
+      else {
+        var fullname   = data.name;
+        var username   = data.login;
+        var reposnum   = data.public_repos;
+        
+        var outhtml = '<ul>'+ "";
+        var repositories;
+        $.getJSON(repourl, function(data){
+          repositories = data;   
+          outputRepo();                
+        });
+        function outputRepo() {
+          if(repositories.length == 0) { 
+          outhtml = outhtml + '<p>No repos!</p></div>'; 
+          }else {
+            outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
+            $.each(repositories, function(index) {
+              outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
+            });
+            outhtml = outhtml + '</ul></div>'; 
+          }
+          $('#ghapidata').html(outhtml);
+        } 
+      } 
+    }); 
+  }); 
